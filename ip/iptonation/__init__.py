ec3364e656cae5d192df81bb42bdf45b68fa5bed
rasca0027 <rasca0027@gmail.com>
2015-08-07 18:21:10 +0800

    fix sql

--- ip/iptonation/__init__.py
+++ ip/iptonation/__init__.py
@@ -3,10 +3,10 @@ import time
 
 time.sleep(10)
 
-db = MySQLdb.connect(host="mysql",port=3306,user="root",passwd="root", db="test")
-cur = db.cursor()
-
 with open('ip2nation.sql', 'r') as fil:
-    #for line in fil.readlines():
-        #cur.execute(line)
-    cur.execute(fil.read())
+    data = fil.read()
+    db = MySQLdb.connect(host="mysql",port=3306,user="root",passwd="root", db="test")
+    cur = db.cursor()
+    for line in fil.readlines():
+        cur.execute(line)
+    #exit(cur.execute('show tables;')
